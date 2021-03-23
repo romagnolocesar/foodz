802613445045cc23388bbf371656459e5243ee15
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-23 12:12:41 -0300

    Criado Projeto Spring

---
+++ src/test/java/com/romagnolocesar/foodz/FoodzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romagnolocesar.foodz;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodzApiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
