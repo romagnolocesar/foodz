865786853ded25fe16f30e891beaefde8b445aaa
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-23 23:05:07 -0300

    3.9 - Adicionando Cozinhas

--- src/main/java/com/romagnolocesar/foodz/jpa/CadastroCozinha.java
+++ src/main/java/com/romagnolocesar/foodz/jpa/CadastroCozinha.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,32 @@ public class CadastroCozinha {
 		TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
 		return query.getResultList();
 	}
+	
+	@Transactional
+	@GetMapping(value="cozinhas/add")
+	public String adicionarCall() {
+		
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Chinesa");
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Pizzas");
+		
+		cozinha1 = this.adicionar(cozinha1);
+		cozinha2 = this.adicionar(cozinha2);
+		
+		System.out.printf("%d - %s \n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s \n", cozinha2.getId(), cozinha2.getNome());
+		
+		return "Cozinha Adicionada!";
+	}
+	
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+		
+//		System.out.println("LOG: Cozinha Adicionada!");
+//		return "Cozinha Adicionada!";
+	}
+	
+	
 }
