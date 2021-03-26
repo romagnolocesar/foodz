22a897e60bceeb70934f594a93969a2bf27b79f1
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-26 20:31:31 -0300

    5.15 Filtros Dinamicos com Criteria

--- src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.romagnolocesar.foodz.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -32,12 +33,24 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 		Root<Restaurante> root = criteria.from(Restaurante.class);
 		
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
-		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
-		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		var predicates = new ArrayList<Predicate>();
 		
-		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+		if(StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
 		
+		if(taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+
+		}
+		
+		if(taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));			
+		}
+
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
+
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
