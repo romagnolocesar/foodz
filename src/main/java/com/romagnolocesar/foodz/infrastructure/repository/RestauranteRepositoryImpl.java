c1f4b1f57ab9115de3203742cfbc93bf3ef04b17
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-26 19:05:49 -0300

    5.13 Usando Criteria API

--- src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -24,34 +26,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
 		
-		var parametros = new HashMap<String, Object>();
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
-		if(StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-		
-		if(taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if(taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
-		
-		
-		parametros.forEach(
-				(String key, Object obj) -> query.setParameter(key, obj)
-		);
-
 		return query.getResultList();
 	}
 }
