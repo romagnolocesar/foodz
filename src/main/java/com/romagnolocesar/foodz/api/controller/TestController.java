3168b0ee8b45874348fc76418f5e7a2ac3ece877
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-26 21:41:57 -0300

    5.19 Injetando o proprio repositorio na impl customizada @Lazy

--- src/main/java/com/romagnolocesar/foodz/api/controller/TestController.java
+++ src/main/java/com/romagnolocesar/foodz/api/controller/TestController.java
@@ -72,8 +72,7 @@ public class TestController {
 	
 	@GetMapping("/restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome){
-		return restauranteRepository.findAll(comFreteGratis()
-				.and(comNomeSemelhante(nome)));
+		return restauranteRepository.findComFreteGratis(nome);
 	}
 	
 	
--- src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/romagnolocesar/foodz/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.romagnolocesar.foodz.infrastructure.repository;
 
+import static com.romagnolocesar.foodz.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.romagnolocesar.foodz.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -13,10 +15,13 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.romagnolocesar.foodz.domain.model.Restaurante;
+import com.romagnolocesar.foodz.domain.repository.RestauranteRepository;
 import com.romagnolocesar.foodz.domain.repository.RestauranteRepositoryQueries;
 
 @Repository
@@ -24,6 +29,9 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 	
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired @Lazy
+	private RestauranteRepository restauranteRepository;
 	
 	@Override
 	public List<Restaurante> find(String nome, 
@@ -55,5 +63,11 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
 		return query.getResultList();
+	}
+
+	@Override
+	public List<Restaurante> findComFreteGratis(String nome) {
+		return restauranteRepository.findAll(comFreteGratis()
+				.and(comNomeSemelhante(nome)));
 	}
 }
